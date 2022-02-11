164109966c02fd9aed3840c5ad4ce681d8a6fcf2
Al-amin Sheikh <azalamins@gmail.com>
2022-02-11 06:20:12 +0600

    added second page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// Login Form 
+document.getElementById('submit').addEventListener('click', function() {
+    const userEmailField = document.getElementById('user-email');
+    const userPasswordField = document.getElementById('user-password');
+    const userEmail = userEmailField.value;
+    const userPassword = userPasswordField.value;
+
+    if (userEmail == "baper@bank.com" && userPassword == "secret") {
+        // window.location.href = '../transaction.html';
+        window.open(
+            '../transaction.html', '_blank' // <- This is what makes it open in a new window.
+        );
+    }
+});
---
+++ js/transaction.js
@@ -0,0 +1,6 @@
+// Bank Transaction
+document.getElementById('deposit-btn').addEventListener('click', function() {
+    const depositField = document.getElementById('deposit-input');
+    const withdrawField = document.getElementById('withdraw-input');
+    console.log(depositField, withdrawField)
+})
